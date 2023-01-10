337765e6c2420993aa4fa1a7f91433f90f1d1352
Deep <deepshkhwt18@gmail.com>
2023-01-10 18:35:52 +0530

    Files Added

---
+++ contact.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int num1 , num2;
+    int res = 0;
+    cout<<"Enter two number :";
+    cin >> num1;
+    cin >> num2;
+    res = num2 + num1;
+    cout<<"Addition of given number is : " << res;
+
+    return 0;
+}
